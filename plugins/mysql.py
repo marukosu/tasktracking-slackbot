16de0a21d6ddc6113c2ef0f88efc8d3f05c78a49
marukosu <24562112+marukosu@users.noreply.github.com>
2017-04-04 09:48:34 +0900

    respond to Japanese input

--- plugins/mysql.py
+++ plugins/mysql.py
@@ -6,7 +6,7 @@ class MySQL:
     def __init__(self, test):
         if test == 1:
             self.engine = create_engine(
-                "mysql://slack:slack@127.0.0.1:13306/slack",
+                "mysql://slack:slack@127.0.0.1:13306/slack?charset=utf8",
                 encoding='utf-8',
                 echo=True,
                 pool_size=10,
@@ -14,7 +14,7 @@ class MySQL:
             )
             return
         self.engine = create_engine(
-            "mysql://root:root@localhost/slack",
+            "mysql://root:root@localhost/slack?charset=utf8",
             encoding='utf-8',
             echo=True,
             pool_size=10,
